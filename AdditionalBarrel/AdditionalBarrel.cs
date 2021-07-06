9c7741c5c7085cee20083acdec49fcb5a6072abd
cityrobo <cityrobo@hotmail.de>
2021-07-06 20:02:06 +0200

    updated the script so that other guns using it don't fire as if they were connected by bluetooth lol

--- AdditionalBarrel/AdditionalBarrel.cs
+++ AdditionalBarrel/AdditionalBarrel.cs
@@ -28,21 +28,24 @@ namespace Cityrobo
         private void FVRFireArm_Fire(On.FistVR.FVRFireArm.orig_Fire orig, FistVR.FVRFireArm self, FistVR.FVRFireArmChamber chamber, UnityEngine.Transform muzzle, bool doBuzz, float velMult)
         {
             orig(self, chamber, muzzle, doBuzz, velMult);
-            float chamberVelMult = AM.GetChamberVelMult(chamber.RoundType, Vector3.Distance(chamber.transform.position, muzzle.position));
-            float num = self.GetCombinedFixedDrop(self.AccuracyClass) * 0.0166667f;
-            Vector2 vector = self.GetCombinedFixedDrift(self.AccuracyClass) * 0.0166667f;
-
-            for (int i = 0; i < chamber.GetRound().NumProjectiles; i++)
+            if (this.fireArm == self)
             {
-                float d = chamber.GetRound().ProjectileSpread + self.m_internalMechanicalMOA + self.GetCombinedMuzzleDeviceAccuracy();
-                if (chamber.GetRound().BallisticProjectilePrefab != null)
+                float chamberVelMult = AM.GetChamberVelMult(chamber.RoundType, Vector3.Distance(chamber.transform.position, muzzle.position));
+                float num = fireArm.GetCombinedFixedDrop(fireArm.AccuracyClass) * 0.0166667f;
+                Vector2 vector = fireArm.GetCombinedFixedDrift(fireArm.AccuracyClass) * 0.0166667f;
+
+                for (int i = 0; i < chamber.GetRound().NumProjectiles; i++)
                 {
-                    Vector3 b = muzzle.forward * 0.005f;
-                    GameObject gameObject = UnityEngine.Object.Instantiate<GameObject>(chamber.GetRound().BallisticProjectilePrefab, this.muzzle.position - b, this.muzzle.rotation);
-                    Vector2 vector2 = (UnityEngine.Random.insideUnitCircle + UnityEngine.Random.insideUnitCircle + UnityEngine.Random.insideUnitCircle) * 0.33333334f * d;
-                    gameObject.transform.Rotate(new Vector3(vector2.x + vector.y + num, vector2.y + vector.x, 0f));
-                    BallisticProjectile component = gameObject.GetComponent<BallisticProjectile>();
-                    component.Fire(component.MuzzleVelocityBase * chamber.ChamberVelocityMultiplier * velMult * chamberVelMult, gameObject.transform.forward, self);
+                    float d = chamber.GetRound().ProjectileSpread + fireArm.m_internalMechanicalMOA + fireArm.GetCombinedMuzzleDeviceAccuracy();
+                    if (chamber.GetRound().BallisticProjectilePrefab != null)
+                    {
+                        Vector3 b = muzzle.forward * 0.005f;
+                        GameObject gameObject = UnityEngine.Object.Instantiate<GameObject>(chamber.GetRound().BallisticProjectilePrefab, this.muzzle.position - b, this.muzzle.rotation);
+                        Vector2 vector2 = (UnityEngine.Random.insideUnitCircle + UnityEngine.Random.insideUnitCircle + UnityEngine.Random.insideUnitCircle) * 0.33333334f * d;
+                        gameObject.transform.Rotate(new Vector3(vector2.x + vector.y + num, vector2.y + vector.x, 0f));
+                        BallisticProjectile component = gameObject.GetComponent<BallisticProjectile>();
+                        component.Fire(component.MuzzleVelocityBase * chamber.ChamberVelocityMultiplier * velMult * chamberVelMult, gameObject.transform.forward, fireArm);
+                    }
                 }
             }
         }
