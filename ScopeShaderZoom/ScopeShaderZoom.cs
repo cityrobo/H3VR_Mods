3373eb7a3c9cf28c991cad7f4205ff02f1773621
cityrobo <cityrobo@hotmail.de>
2021-07-26 03:33:57 +0200

    Added ScopeShaderZoom script. DLL in Releases folder

---
+++ ScopeShaderZoom/ScopeShaderZoom.cs
@@ -0,0 +1,80 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using FistVR;
+namespace Cityrobo
+{
+    public class ScopeShaderZoom : MonoBehaviour
+    {
+        public FVRInteractiveObject AttachmentInterface;
+        public MeshRenderer scopeLens;
+        public Camera camera;
+        public List<float> ZoomFactor;
+
+        [Header("If you want a Screen above the scope that shows the current Magninification, use this:")]
+        public Text text;
+
+        private List<float> CorrespondingCameraFOV;
+
+
+        private int currentZoomIndex;
+        private bool hasZoomText;
+        public void Start()
+        {
+            CorrespondingCameraFOV = new List<float>();
+            Debug.Log(camera.ToString());
+
+            currentZoomIndex = 0;
+            if (text != null) hasZoomText = true;
+            else hasZoomText = false;
+
+            for (int i = 0; i < ZoomFactor.Count; i++)
+            {
+                CorrespondingCameraFOV.Add(53.7f * Mathf.Pow(ZoomFactor[i], -0.9284f) - 0.5035f);
+            }
+            Debug.Log(CorrespondingCameraFOV.ToString());
+        }
+        
+        public void Update()
+        {
+            FVRViveHand hand = AttachmentInterface.m_hand;
+            if (hand != null)
+            {
+                if (hand.Input.TouchpadDown && Vector2.Angle(hand.Input.TouchpadAxes, Vector2.left) < 45f) PreviousZoom();
+                else if (hand.Input.TouchpadDown && Vector2.Angle(hand.Input.TouchpadAxes, Vector2.right) < 45f) NextZoom();
+
+                if (hasZoomText)
+                {
+                    text.gameObject.SetActive(true);
+                    text.text = string.Format("Zoom: {0}x", ZoomFactor[currentZoomIndex].ToString());
+                }
+            }
+            else if (hasZoomText) text.gameObject.SetActive(false);
+        }
+
+        public void NextZoom()
+        {
+            if (currentZoomIndex == ZoomFactor.Count - 1) return;
+            currentZoomIndex++;
+            SetZoom();
+        }
+
+        public void PreviousZoom()
+        {
+            if (currentZoomIndex == 0) return;
+            currentZoomIndex--;
+            SetZoom();
+        }
+
+        public void SetZoom()
+        {
+            camera.fieldOfView = CorrespondingCameraFOV[currentZoomIndex];
+        }
+
+        public void ChangeElevation()
+        {
+            
+        }
+    }
+}
+
