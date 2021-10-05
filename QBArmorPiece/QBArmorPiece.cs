6425d27d583da4e66f1403b65bee9b736a802362
cityrobo <cityrobo@hotmail.de>
2021-10-05 15:31:02 +0200

    added QBArmorPiece scripts. compiled monomod DLL inside Releases folder

---
+++ QBArmorPiece/QBArmorPiece.cs
@@ -0,0 +1,32 @@
+using FistVR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+namespace Cityrobo
+{
+    public class QBArmorPiece : FVRPhysicalObject
+    {
+#if!(UNITY_EDITOR || UNITY_5)
+		public override void SetQuickBeltSlot(FVRQuickBeltSlot slot)
+		{
+			if (this.m_quickbeltSlot != null && slot != this.m_quickbeltSlot)
+			{
+				this.m_quickbeltSlot.HeldObject = null;
+				this.m_quickbeltSlot.CurObject = null;
+				this.m_quickbeltSlot.IsKeepingTrackWithHead = false;
+			}
+			if (slot != null && !base.IsHeld)
+			{
+				slot.HeldObject = this;
+				slot.CurObject = this;
+				slot.IsKeepingTrackWithHead = this.DoesQuickbeltSlotFollowHead;
+			}
+			this.m_quickbeltSlot = slot;
+		}
+#endif
+	}
+}
