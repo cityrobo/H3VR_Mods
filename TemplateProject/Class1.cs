623e3d5aa5fb9b441435f651031409e39dfac02b
cityrobo <cityrobo@hotmail.de>
2022-01-10 03:50:05 +0100

    Added Template project that you can use to start making your own scripts!

---
+++ TemplateProject/Class1.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using FistVR;
+
+namespace Cityrobo
+{
+    public class Class1
+    {
+    }
+}
