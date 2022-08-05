8ca8a842d732375ee99c330aab452ee0fbe692a1
cityrobo <84413175+cityrobo@users.noreply.github.com>
2022-08-05 20:20:45 +0200

    had to update the bepinex plugin to add config support

--- OpenScriptsBepInEx/OpenScripts_BepInEx.cs
+++ OpenScriptsBepInEx/OpenScripts_BepInEx.cs
@@ -5,36 +5,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using BepInEx;
+using BepInEx.Configuration;
 
 namespace Cityrobo
 {
     [BepInPlugin("h3vr.cityrobo.OpenScripts", "OpenScripts BepInEx Loader Plugin", "1.0.0")]
-    class OpenScripts_BepInEx : BaseUnityPlugin
+    public class OpenScripts_BepInEx : BaseUnityPlugin
     {
         public static string PluginPath;
+        public static OpenScripts_BepInEx Instance;
 
         public static List<string> LoadedPluginPaths = new List<string>();
+
+        // FirearmHeatingEffect Config Entries:
+        public static ConfigEntry<bool> FirearmHeatingEffect_CanExplode;
+        public static ConfigEntry<bool> FirearmHeatingEffect_CanRecover;
+        public static ConfigEntry<float> FirearmHeatingEffect_RecoverThreshold;
         public OpenScripts_BepInEx()
         {
-            PluginPath = this.Info.Location;
-            //Logger.LogInfo($"PluginPath: {PluginPath}");
+            Instance = this;
+            // FirearmHeatingEffect Config Bindings
+            FirearmHeatingEffect_CanExplode = Config.Bind("Firearm Heating Effect", "Part can explode", true, "If true, and the part is setup to do so, the parts with heating effects can explode.");
+            FirearmHeatingEffect_CanRecover = Config.Bind("Firearm Heating Effect", "Part can recover", false, "If true, parts can recover from being exploded.");
+            FirearmHeatingEffect_RecoverThreshold = Config.Bind("Firearm Heating Effect", "Recover heat threshold", 0f, new ConfigDescription("Defines the heat value, at which the part will recover from being exploded", new AcceptableValueRange<float>(0, 1)));
 
+            PluginPath = this.Info.Location;
             string pluginName = Path.GetFileName(PluginPath);
-            //Logger.LogInfo($"pluginName: {pluginName}");
             string pluginFolder = Path.GetDirectoryName(PluginPath);
-            //Logger.LogInfo($"pluginFolder: {pluginFolder}");
 
             DirectoryInfo directoryInfo = new DirectoryInfo(pluginFolder);
             FileInfo[] filesInDir = directoryInfo.GetFiles("*.dll");
 
             foreach (FileInfo file in filesInDir)
             {
-                //Logger.LogInfo($"file.FullName: {file.FullName}");
                 if (file.FullName == PluginPath) continue;
                 System.Reflection.Assembly.LoadFrom(file.FullName);
                 LoadedPluginPaths.Add(file.FullName);
             }
-            Logger.LogInfo("OpenScripts loaded!");
+            Logger.LogInfo("Fully loaded all OpenScripts DLLs!");
         }
     }
 }
